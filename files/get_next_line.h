35840ed1a16126a784318673bf32f15b001f1c1d
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2025-09-09 11:37:12 -0300

    Create get_next_line.h

---
+++ files/get_next_line.h
@@ -0,0 +1,18 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strjoin_free(char *s1, const char *s2);
+
+#endif
