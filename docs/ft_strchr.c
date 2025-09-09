3d1f77a591b489b1711d97f43509b542eef1a562
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2025-09-09 09:58:43 -0300

    Create ft_strchr.c

---
+++ docs/ft_strchr.c
@@ -0,0 +1,14 @@
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
