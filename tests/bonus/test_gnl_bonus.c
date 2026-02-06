8e9012d13937b29eaab0a63241b73ad8a5d83f06
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2026-02-06 18:26:19 -0300

    Create test_gnl_bonus.c

---
+++ tests/bonus/test_gnl_bonus.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_gnl_bonus.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: twippel- <twippel-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 00:00:00 by twippel-          #+#    #+#             */
+/*   Updated: 2025/02/06 00:00:00 by twippel-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// TODO: Implement when bonus is ready
+
+/*
+#include "get_next_line_bonus.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+**
+ * Bonus test suite for multiple file descriptors
+ * 
+ * Tests:
+ * - Reading from multiple FDs simultaneously
+ * - Interleaving reads
+ * - Memory management with multiple FDs
+ * - Edge cases with FD limits
+ *
+
+void	test_multiple_fds(void)
+{
+	// TODO: Test multiple file descriptors
+}
+
+void	test_interleaved_reads(void)
+{
+	// TODO: Test reading from FDs in different orders
+}
+
+void	test_many_fds(void)
+{
+	// TODO: Test with many file descriptors (stress test)
+}
+
+int	main(void)
+{
+	printf("\n╔════════════════════════════════════════╗\n");
+	printf("║   Get Next Line - BONUS Test Suite    ║\n");
+	printf("╚════════════════════════════════════════╝\n\n");
+
+	test_multiple_fds();
+	test_interleaved_reads();
+	test_many_fds();
+
+	return (0);
+}
+*/
