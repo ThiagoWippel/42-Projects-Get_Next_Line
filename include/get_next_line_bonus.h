cd2e77e9f8b871a5b8bb06519e2c93df46b2815f
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2026-02-06 19:46:38 -0300

    Update get_next_line_bonus.h

--- include/get_next_line_bonus.h
+++ include/get_next_line_bonus.h
@@ -1,1 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: twippel- <twippel-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 00:00:00 by twippel-          #+#    #+#             */
+/*   Updated: 2025/02/06 00:00:00 by twippel-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# ifndef FD_MAX
+#  define FD_MAX 1024
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strjoin_free(char *s1, const char *s2);
+
+#endif
